67e7aad38187a74408e5916aff0bb5ef6aa5a835
Yash Upadhyay <yash.upadhyay.2723@gmail.com>
2023-08-06 07:59:23 +0530

    Initial Product Service Created

---
+++ product-service/src/main/java/com/yash/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.yash.productservice.repository;
+
+import com.yash.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
