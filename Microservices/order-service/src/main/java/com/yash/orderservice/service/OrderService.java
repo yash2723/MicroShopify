8796ded27ed43ce9c3ee4b2c1729d9efd88e4048
Yash Upadhyay <yash.upadhyay.2723@gmail.com>
2023-08-29 09:07:22 +0530

    Inter Service Communication using WebClient

--- Microservices/order-service/src/main/java/com/yash/orderservice/service/OrderService.java
+++ Microservices/order-service/src/main/java/com/yash/orderservice/service/OrderService.java
@@ -7,6 +7,7 @@ import com.yash.orderservice.model.OrderLineItems;
 import com.yash.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -26,7 +28,18 @@ public class OrderService {
                         .map(this::mapToDto)
                         .collect(Collectors.toList())
         );
-        orderRepository.save(order);
+        Boolean isAvailable = webClient.get()
+                .uri("http://localhost:8082/api/inventory")
+                .retrieve()
+                .bodyToMono(Boolean.class)
+                .block();
+
+        if(isAvailable) {
+            orderRepository.save(order);
+        }
+        else {
+            throw new IllegalArgumentException("Product is not in stock. Please try again later.");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto e) {
