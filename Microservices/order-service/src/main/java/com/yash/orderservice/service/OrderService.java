6c5e3ceb6beadaeaf9b4aee3292ae20e085f1874
Yash Upadhyay <yash.upadhyay.2723@gmail.com>
2023-09-01 19:51:22 +0530

    API Gateway Integration

--- Microservices/order-service/src/main/java/com/yash/orderservice/service/OrderService.java
+++ Microservices/order-service/src/main/java/com/yash/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -35,8 +35,8 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemsList().stream()
                 .map(OrderLineItems::getSkuCode)
                 .toList();
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         urlBuilder -> urlBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
