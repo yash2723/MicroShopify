cbe99d10a07b3ab63b9a102dbd3442aabf89958d
Yash Upadhyay <yash.upadhyay.2723@gmail.com>
2023-09-02 20:49:26 +0530

    Circuit breaker implemented in Order Service

--- Microservices/order-service/src/main/java/com/yash/orderservice/controller/OrderController.java
+++ Microservices/order-service/src/main/java/com/yash/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.yash.orderservice.controller;
 
 import com.yash.orderservice.dto.OrderRequest;
 import com.yash.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,13 @@ public class OrderController {
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully.";
     }
 
+    public String fallBackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return "Oops! Something Went Wrong. Please Order After Some Time.";
+    }
 }
